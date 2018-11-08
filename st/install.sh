7969ce50b227d40f05477053d8bf250d7d969b42
zrhoffman <zrhoffman@ku.edu>
2018-11-08 10:42:08 -0600

    only download st directory if it does not already exist

--- st/install.sh
+++ st/install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 #Assumes Arch Linux and yay AUR package installed
-yay --getpkgbuild st;
+if [[ ! -d st ]];
+then
+    yay --getpkgbuild st;
+fi;
+
 sha256=$(openssl sha256 config.h | awk '{print $2}');
 pushd st || exit;
+git pull;
 cp ../config.h .;
 sed -i '16s/.*/'"'$sha256')"'/' PKGBUILD;
+rm -f *.*z;
 makepkg;
 makepkg --install --noconfirm;
 popd || exit;
-rm -rf st;
